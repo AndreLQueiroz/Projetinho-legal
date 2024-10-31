96d57e2c96964da0a9dedfe09232bc1a653bc4a6
Andre <andrezicadobaile1@gmail.com>
2024-10-31 08:56:41 -0300

    adicionando ao github

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Nav from "./components/Nav/Nav"
+import Footer from "./components/Footer/Footer"
+function App() {
+  
+
+  return (
+    <>
+     <Nav/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import Home from './routes/Home/Home.jsx'
+import Error from './routes/Error/Error.jsx'
+import Produtos from './routes/Produtos/Produtos.jsx'
+import Login from './routes/Login/Login.jsx'
+
+
+
+const router = createBrowserRouter([{
+  path:"/", element:<App/>,
+  errorElement:<Error/>,
+
+  children:[
+    {path:"/", element:<Home/>},
+    {path:"/produtos", element:<Produtos/>},
+    {path:"/login", element:<Login/>}
+  ]
+}])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router ={router} />
+  </StrictMode>,
+)
